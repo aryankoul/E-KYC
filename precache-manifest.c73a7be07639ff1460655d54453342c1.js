d2c7d0b75a4bb451efc6292fb51e66c3210b0746
aryankoul <aryankoul10@gmail.com>
2020-05-30 00:24:07 +0530

    Updates

---
+++ precache-manifest.c73a7be07639ff1460655d54453342c1.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "0d39bd7d47debc8e6974",
+    "url": "/E-KYC/static/css/main.662b4c25.chunk.css"
+  },
+  {
+    "revision": "0d39bd7d47debc8e6974",
+    "url": "/E-KYC/static/js/main.0d39bd7d.chunk.js"
+  },
+  {
+    "revision": "d5d27705c9a7faea0bbd",
+    "url": "/E-KYC/static/css/1.7b09487b.chunk.css"
+  },
+  {
+    "revision": "d5d27705c9a7faea0bbd",
+    "url": "/E-KYC/static/js/1.d5d27705.chunk.js"
+  },
+  {
+    "revision": "ebb69eef604d4f55c11a",
+    "url": "/E-KYC/static/js/runtime~main.ebb69eef.js"
+  },
+  {
+    "revision": "c9643d62892ba24fb9c8e96a2344cb89",
+    "url": "/E-KYC/index.html"
+  }
+];
