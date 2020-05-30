03825088dfa35a932d7122f0a902abb148394738
aryankoul <aryankoul10@gmail.com>
2020-05-30 13:10:59 +0530

    Updates

---
+++ precache-manifest.58b9e5cb41c66c29de5b546b56ee57a2.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "5df95b93cb40821c1fab",
+    "url": "/E-KYC/static/css/main.662b4c25.chunk.css"
+  },
+  {
+    "revision": "5df95b93cb40821c1fab",
+    "url": "/E-KYC/static/js/main.5df95b93.chunk.js"
+  },
+  {
+    "revision": "e0b5bfa26f08e512a0c5",
+    "url": "/E-KYC/static/css/1.7b09487b.chunk.css"
+  },
+  {
+    "revision": "e0b5bfa26f08e512a0c5",
+    "url": "/E-KYC/static/js/1.e0b5bfa2.chunk.js"
+  },
+  {
+    "revision": "ebb69eef604d4f55c11a",
+    "url": "/E-KYC/static/js/runtime~main.ebb69eef.js"
+  },
+  {
+    "revision": "081e37fec543dfb125590aafca5f50e0",
+    "url": "/E-KYC/index.html"
+  }
+];
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/E-KYC/precache-manifest.103b0c4c299a4bc6af6c286d6dd4221b.js"
+  "/E-KYC/precache-manifest.58b9e5cb41c66c29de5b546b56ee57a2.js"
 );
 
 workbox.clientsClaim();
